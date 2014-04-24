e2c1d8b45ec5ea247dd9cfede378b5e88ad86418
Defoe Obie <labresch@gmail.com>
2014-04-23 20:55:27 -0700

    * Canvas.cs: Add Checkered Background
    * Layer.cs: Fixing Things up a Little.

--- FantasyMapCreatorFinal/Canvas.cs
+++ FantasyMapCreatorFinal/Canvas.cs
@@ -10,19 +10,16 @@ namespace FantasyMapCreatorFinal
     {
         private int height, width;
         private int x_offset, y_offset;
-       
         private ImageSurface workingSurface;
-        private PointD lastpoint;
+        private PointD lastpoint, firstpoint;
         private bool layerRenderNeeded;
         private List<Layer> layers;
-        
         //Temporary
-        private Queue q;
         private Layer testLayer;
         //Properties
         public int X{ get { return x_offset; } }
+
         public int Y{ get { return y_offset; } }
-        
         //Constructor
         public Canvas(int width, int height) : base()
         {
@@ -36,47 +33,47 @@ namespace FantasyMapCreatorFinal
             
             workingSurface = new Cairo.ImageSurface(Cairo.Format.ARGB32, width, height);
             
-            q = new Queue();
             layers = new List<Layer>();
             testLayer = new Layer(width, height);
-            layerRenderNeeded = false;
+            layerRenderNeeded = true;
             
             
             this.AddEvents((int)Gdk.EventMask.ButtonPressMask);
             this.AddEvents((int)Gdk.EventMask.Button1MotionMask);
+            this.AddEvents((int)Gdk.EventMask.Button3MotionMask);
             this.AddEvents((int)Gdk.EventMask.ButtonReleaseMask);
         }
 
-        private void drawOnSurface(ImageSurface surface)
-        {
-            Cairo.Context cr = new Context(surface);
-            cr.SetSourceRGB(0, 0.2, 0.6);
-            cr.LineJoin = Cairo.LineJoin.Round;
-            
-            if (q.Count > 2)
-            {
-                PointD previous = (PointD)q.Dequeue();
-                PointD current; //= (PointD)q.Dequeue();
-                PointD next;
-                cr.MoveTo(previous);
-                while (q.Count > 1)
-                {
-                    current = (PointD)q.Dequeue();
-                    next = (PointD)q.Dequeue();
-                    cr.CurveTo(previous,current, next);
-                    previous = next;
-                    //current = next;
-                }
-                if (q.Count > 0)
-                    cr.LineTo((PointD)q.Dequeue());
-            }
-            
-            cr.Stroke();
-            ((IDisposable)cr.GetTarget()).Dispose();
-            ((IDisposable)cr).Dispose();
-            
-            q.Clear();
-        }
+//        private void drawOnSurface(ImageSurface surface)
+//        {
+//            Cairo.Context cr = new Context(surface);
+//            cr.SetSourceRGB(0, 0.2, 0.6);
+//            cr.LineJoin = Cairo.LineJoin.Round;
+//            
+//            if (q.Count > 2)
+//            {
+//                PointD previous = (PointD)q.Dequeue();
+//                PointD current; //= (PointD)q.Dequeue();
+//                PointD next;
+//                cr.MoveTo(previous);
+//                while (q.Count > 1)
+//                {
+//                    current = (PointD)q.Dequeue();
+//                    next = (PointD)q.Dequeue();
+//                    cr.CurveTo(previous, current, next);
+//                    previous = next;
+//                    //current = next;
+//                }
+//                if (q.Count > 0)
+//                    cr.LineTo((PointD)q.Dequeue());
+//            }
+//            
+//            cr.Stroke();
+//            ((IDisposable)cr.GetTarget()).Dispose();
+//            ((IDisposable)cr).Dispose();
+//            
+//            q.Clear();
+//        }
 
         protected override bool OnExposeEvent(Gdk.EventExpose evnt)
         {
@@ -86,10 +83,6 @@ namespace FantasyMapCreatorFinal
             int laheight = this.Parent.Allocation.Height;
             this.x_offset = lawidth > width ? (lawidth - width) / 2 : 0;
             this.y_offset = laheight > height ? (laheight - height) / 2 : 0;
-            //int finalwidth = Math.Min(lawidth, width);
-            ImageSurface surface = new Cairo.ImageSurface(Cairo.Format.ARGB32, width, height);
-            
-            //drawOnSurface(surface);
             
             Console.WriteLine("EXPOSE EVENT");
             using (Cairo.Context gr = Gdk.CairoHelper.Create(this.GdkWindow))
@@ -97,22 +90,45 @@ namespace FantasyMapCreatorFinal
                 // Rectangle r = new Rectangle(0, 0, Math.Min(lawidth, width), Math.Min(laheight, height));
                 // gr.Rectangle(r);
                 // gr.Clip();)
-                
-                gr.Rectangle(x_offset - 2, y_offset - 2, width + 4, height + 4);
-                gr.SetSourceRGB(0, 0, 0);
+                ImageSurface slfjds = new Cairo.ImageSurface(Cairo.Format.A8, 20, 20);
+                Cairo.Context la = new Cairo.Context(slfjds);
+                la.SetSourceRGBA(0, 0, 0, 0.1);
+                la.Paint();
+                la.Rectangle(0, 0, 10, 10);
+                la.Rectangle(10, 10, 20, 20);
+                la.SetSourceRGBA(0, 0, 0, 0.2);
+                la.Fill();
+                ((IDisposable)la.GetTarget()).Dispose();
+                ((IDisposable)la).Dispose();
+                Pattern checkers = new SurfacePattern(slfjds);
+                checkers.Extend = Cairo.Extend.Repeat;
+                //gr.SetSource(slfjds);
+                gr.Mask(checkers);
+                //gr.Paint();
+                gr.Rectangle(x_offset - 1, y_offset - 1, width + 2, height + 2);
+                gr.SetSourceRGBA(0, 0, 0, 0.9);
                 gr.Fill();
+                gr.SetSourceRGBA(0, 0, 0, 0.2);
+                gr.Rectangle(x_offset + 2, y_offset + 2, width, height);
+                gr.FillPreserve();
+                gr.Rectangle(x_offset + 4, y_offset + 4, width, height);
+                gr.FillPreserve();
+                gr.Rectangle(x_offset + 6, y_offset + 6, width, height);
+                gr.Fill();
+                
                 //Temporary  white backgroundage.
                 gr.Rectangle(x_offset, y_offset, width, height);
                 gr.SetSourceRGB(1, 1, 1);
                 gr.Fill();
                 if (layerRenderNeeded)
                 {
-                    testLayer.RenderLayer(surface);
-                    gr.SetSourceSurface(surface, x_offset, y_offset);
+                    testLayer.RenderLayer();
+                    gr.SetSourceSurface(testLayer, x_offset, y_offset);
                     gr.Paint();
-                    layerRenderNeeded = false;
+                    workingSurface = new Cairo.ImageSurface(Cairo.Format.Argb32, width, height);
                 }
-                else{
+                else
+                {
                     gr.SetSourceSurface(workingSurface, x_offset, y_offset);
                     gr.Paint();
                 }
@@ -126,59 +142,70 @@ namespace FantasyMapCreatorFinal
 
         protected override bool OnButtonPressEvent(Gdk.EventButton evnt)
         {
-            base.OnButtonPressEvent(evnt);
-            System.Console.WriteLine("On button press event");
-            lastpoint = new PointD(evnt.X - x_offset, evnt.Y - y_offset);
-                 testLayer.AddToPath(lastpoint, Layer.NextPointFunction.MoveTo);
-           
+            //base.OnButtonPressEvent(evnt);
+           // Left Mouse Button
+            if (evnt.Button == 1)
+            {
+                System.Console.WriteLine("On button press event");
+                firstpoint = new PointD(evnt.X - x_offset + 0.5, evnt.Y - y_offset + 0.5);
+                lastpoint = firstpoint;
+                testLayer.AddToPath(firstpoint, Layer.NextPointFunction.MoveTo);
+                layerRenderNeeded = false;
+            }
             return true;
         }
 
         protected override bool OnButtonReleaseEvent(Gdk.EventButton evnt)
         {
-            base.OnButtonReleaseEvent(evnt);
-            layerRenderNeeded = true;
-            this.QueueDraw();
+            if (evnt.Button == 1)
+            {
+                testLayer.AddToPath(firstpoint, Layer.NextPointFunction.LineTo);
+                layerRenderNeeded = true;
+                this.QueueDraw();
+            }
             return true;
         }
 
         protected override bool OnMotionNotifyEvent(Gdk.EventMotion evnt)
         {
-            base.OnMotionNotifyEvent(evnt);
+            // base.OnMotionNotifyEvent(evnt);
             //double drawx = evnt.X - x;
             //double drawy = evnt.Y - y;
             
-            PointD current = new PointD(evnt.X-x_offset, evnt.Y-y_offset);
-            Cairo.Context cr = new Context(workingSurface);
+            if (evnt.State == Gdk.ModifierType.Button1Mask)
+            {
+           
+                PointD current = new PointD(evnt.X - x_offset + 0.5, evnt.Y - y_offset + 0.5);
+                Cairo.Context cr = new Context(workingSurface);
             
-            testLayer.AddToPath(current, Layer.NextPointFunction.LineTo);
+                testLayer.AddToPath(current, Layer.NextPointFunction.LineTo);
             
-            cr.MoveTo(lastpoint.X + 0.5, lastpoint.Y+0.5);
+                cr.MoveTo(lastpoint.X, lastpoint.Y);
+                cr.LineTo(current.X, current.Y);
+                cr.LineJoin = Cairo.LineJoin.Round;
+                cr.LineWidth = 1;
+                cr.SetSourceRGBA(1, 0, 0, 0.8);
+                cr.Stroke();
+                
+                ((IDisposable)cr.GetTarget()).Dispose();
+                ((IDisposable)cr).Dispose();
             
-            cr.LineTo(current.X + 0.5, current.Y + 0.5);
-            cr.LineJoin = Cairo.LineJoin.Round;
+                int leftx = (int)Math.Min(current.X, lastpoint.X) - 1 + x_offset;
+                int upx = (int)Math.Min(current.Y, lastpoint.Y) - 1 + y_offset;
+                int boundingx = (int)Math.Abs(current.X - lastpoint.X) + 2;
+                int boundingy = (int)Math.Abs(current.Y - lastpoint.Y) + 2;
+                GdkWindow.InvalidateRect(new Gdk.Rectangle(leftx, upx, boundingx, boundingy), false);
             
-            //cr.SetSourceRGBA(1, 0, 0,0.3);
-            //cr.LineWidth = 3;
-            //cr.StrokePreserve();
+                //System.Console.WriteLine(String.Format("({0},{1}) is better.", evnt.X-x_offset, evnt.Y-y_offset));
             
-            cr.SetSourceRGBA(1, 0, 0,0.8);
-            cr.LineWidth = 1;
-            cr.Stroke();
-            ((IDisposable)cr.GetTarget()).Dispose();
-            ((IDisposable)cr).Dispose();
-            
-            int leftx = (int)Math.Min(current.X, lastpoint.X) - 1 + x_offset;
-            int upx = (int)Math.Min(current.Y, lastpoint.Y) - 1 + y_offset;
-            int boundingx = (int)Math.Abs(current.X - lastpoint.X) + 2;
-            int boundingy = (int)Math.Abs(current.Y - lastpoint.Y) + 2;
-            GdkWindow.InvalidateRect(new Gdk.Rectangle(leftx, upx, boundingx, boundingy),false);
-            
-            //System.Console.WriteLine(String.Format("({0},{1}) is better.", evnt.X-x_offset, evnt.Y-y_offset));
-            
-            lastpoint = current;
-            q.Enqueue(lastpoint);
+                lastpoint = current;
+            }
+            else if(evnt.State == Gdk.ModifierType.Button3Mask)
+                {
+                Console.WriteLine("RIGHT CLICK!");
+            }
             return true;
+                
         }
     }
 }
--- FantasyMapCreatorFinal/Layer.cs
+++ FantasyMapCreatorFinal/Layer.cs
@@ -25,12 +25,12 @@ namespace FantasyMapCreatorFinal
     {
         // Properties
         private List<PathNode> surfacePaths;
+        private Surface mask;
         // Enums
         public enum NextPointFunction
         {
             MoveTo,
             LineTo}
-
         ;
         // Constructors
         public Layer(string filename) : base(filename)
@@ -50,6 +50,7 @@ namespace FantasyMapCreatorFinal
         // Private Methods
         private void initialize()
         {
+            mask = new ImageSurface(Cairo.Format.A8, this.Width, this.Height);
             surfacePaths = new List<PathNode>();
         }
         // Public methods
@@ -60,10 +61,13 @@ namespace FantasyMapCreatorFinal
             surfacePaths.Add(newNode);
         }
 
-        public void RenderLayer(ImageSurface renderSurface)
+        public void RenderLayer()
         {
+            
             if (surfacePaths.Count > 0)
             {
+                ImageSurface renderSurface = new Cairo.ImageSurface(this.Format, Width, Height);
+                
                 Cairo.Context cr = new Context(renderSurface);
                 Console.WriteLine("HErE");
                 cr.LineWidth = 2;
@@ -86,15 +90,31 @@ namespace FantasyMapCreatorFinal
                 int contournumber = 3;
                 int contourwidth = 2;
                 
-                cr.Antialias = Cairo.Antialias.Default;
-                cr.LineCap = Cairo.LineCap.Round;
-                cr.LineJoin = Cairo.LineJoin.Round;
                 // contourWidth = width of the actual contour lines
                 // contourNumber = number of contours outside the island edges
                 // contourSpacing = spacing between contours
                 
                 // must be drawn from largest to smallest
                 // most intuitive
+                //cr.LineWidth = 10;
+                //cr.SetSourceRGBA(0, 0, 0, 1);
+                //cr.StrokePreserve();
+                cr.Antialias = Cairo.Antialias.None;
+                cr.SetSourceRGBA(1, 1, 1, 1);
+                cr.FillPreserve();
+                
+                Cairo.Context cr2 = new Cairo.Context(mask);
+                cr2.SetSourceRGBA(0, 0, 0, 1);
+                cr2.MaskSurface(renderSurface, 0, 0);
+                
+                ((IDisposable)cr2.GetTarget()).Dispose();
+                ((IDisposable)cr2).Dispose();
+                
+                cr.LineWidth = contourwidth;
+                cr.Antialias = Cairo.Antialias.Default;
+                cr.LineCap = Cairo.LineCap.Round;
+                cr.LineJoin = Cairo.LineJoin.Round;
+                
                 for (int i = contournumber; i > 0; --i)
                 {
                     cr.LineWidth = i * (contourwidth * 2 + contourspacing) + contourwidth;
@@ -105,12 +125,21 @@ namespace FantasyMapCreatorFinal
                     cr.StrokePreserve();
                     
                 }
+                
+                //cr.SetSourceRGBA(0.8, 0.7, 0.11, 0.5);
                 cr.LineWidth = contourwidth;
                 cr.SetSourceRGBA(0, 0, 0, 1);
-                cr.StrokePreserve();
-                cr.SetSourceRGBA(0.8, 0.7, 0.11, 0.5);
-                cr.Fill();
-                //     cr.Stroke();
+                cr.Stroke();
+                
+                cr.SetSourceRGBA(1, 1, 1, 1);
+                cr.MaskSurface(mask, 0, 0);
+                
+                Cairo.Context cr3 = new Cairo.Context(this);
+                cr3.SetSourceSurface(renderSurface, 0, 0);
+                cr3.Paint();
+                ((IDisposable)cr3.GetTarget()).Dispose();
+                ((IDisposable)cr3).Dispose();
+                
                 ((IDisposable)cr.GetTarget()).Dispose();
                 ((IDisposable)cr).Dispose(); 
             }
